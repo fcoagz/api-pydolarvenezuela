ac3bbaecd5ae9ffa69dcd7a3a273fc7ac2950204
fcoagz <grihardware@gmail.com>
2023-08-19 02:22:55 -0400

    ~

--- app.py
+++ app.py
@@ -28,24 +28,26 @@ def get_monitor_by_section(section_dollar: str):
 def get_monitor_by_section_and_key(section_dollar: str, key_monitor: str):
     return jsonify(Api().categorize_monitors(section_dollar, key_monitor))
 
-@app.route('/api/v1/dollar/td/<string:value>/<string:key_monitor>', methods=["GET"])
+@app.route('/api/v1/dollar/tb/<string:value>/<string:key_monitor>', methods=["GET"])
 def convertion_to_dollar(value: str, key_monitor: str):
     try:
         float(value)
 
         price = api.get_dollar(key_monitor)
-        return jsonify({'value_to_bs': value, 'value_to_dollar': float(value) * float(price) if not type(price) == dict
-                else price})
+        result = float(value) * float(price) if not type(price) == dict else price
+        return jsonify({'value_to_bs': result, "information": api.get_monitor(key_monitor)})
     except ValueError:
         return jsonify({'message': f'Cannot GET information the value: {value}'})
     
-@app.route('/api/v1/dollar/tb/<string:value>/<string:key_monitor>', methods=["GET"])
+@app.route('/api/v1/dollar/td/<string:value>/<string:key_monitor>', methods=["GET"])
 def convertion_to_bs(value: str, key_monitor: str):
     try:
         float(value)
 
         price = api.get_dollar(key_monitor)
-        return jsonify({'value_to_dollar': value, 'value_to_bs': float(value) / float(price) if not type(price) == dict
-                else price})
+        result = float(value) / float(price) if not type(price) == dict else price
+        return jsonify({'value_to_dollar': result, "information": api.get_monitor(key_monitor)})
     except ValueError:
         return jsonify({'message': f'Cannot GET information the value: {value}'})
+    
+app.run()

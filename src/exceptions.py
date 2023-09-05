0062dc147239eeff8056035696939a56f6d12be0
fcoagz <grihardware@gmail.com>
2023-09-05 18:43:11 -0400

    adding the status code of the exceptions

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,7 +1,11 @@
 from flask import jsonify
 
 def page_not_found(e):
-    return jsonify({"error": "Sorry, the page you were looking for could not be found."}), 404
+    message = jsonify({"error": "Sorry, the page you were looking for could not be found."})
+    message.status_code = 404
+    return message
 
 def internal_server_error(e):
-    return jsonify({"error": "Sorry, an internal problem has occurred on the server. Please try again later."}), 500
+    message = jsonify({"error": "Sorry, an internal problem has occurred on the server. Please try again later."})
+    message.status_code = 500
+    return message

5a4df8701abfc6658ddb94d2316988ed212e2015
fcoagz <grihardware@gmail.com>
2023-09-05 18:42:36 -0400

    get values ​​before returning

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, jsonify 
+from flask import Flask, jsonify
 from flask_cors import CORS
 from src.obtaining import Api
 from src.exceptions import page_not_found, internal_server_error
 
 api = Api()
-
 app = Flask(__name__)
 CORS(app)
 
@@ -18,19 +17,23 @@ def index():
 
 @app.route('/api/v1/dollar/', methods=["GET"])
 def get_monitors():
-    return jsonify(api.get_all_monitors())
+    response = jsonify(api.get_all_monitors())
+    return response
 
 @app.route('/api/v1/dollar/unit/<string:key_monitor>', methods=["GET"])
 def get_by_monitor(key_monitor: str):
-    return jsonify(api.get_monitor(key_monitor))
+    response = jsonify(api.get_monitor(key_monitor))
+    return response
 
 @app.route('/api/v1/dollar/<string:section_dollar>', methods=["GET"])
 def get_monitor_by_section(section_dollar: str):
-    return jsonify(api.categorize_monitors(section_dollar))
+    response = jsonify(api.categorize_monitors(section_dollar))
+    return response
 
 @app.route('/api/v1/dollar/<string:section_dollar>/<string:key_monitor>', methods=["GET"])
 def get_monitor_by_section_and_key(section_dollar: str, key_monitor: str):
-    return jsonify(api.categorize_monitors(section_dollar, key_monitor))
+    response = jsonify(api.categorize_monitors(section_dollar, key_monitor))
+    return response
 
 @app.route('/api/v1/dollar/tb/<string:value>/<string:key_monitor>', methods=["GET"])
 def convertion_to_dollar(value: str, key_monitor: str):

e4410b7f77ecaf5824a5645636938e4ba0c24aa3
fcoagz <grihardware@gmail.com>
2023-09-29 22:46:29 -0400

    ~

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from src.obtaining import Api
+
+from src.obtaining import NewApi
 from src.exceptions import page_not_found, internal_server_error
 
-api = Api()
+api = NewApi()
 app = Flask(__name__)
 CORS(app)
 
@@ -22,37 +23,15 @@ def get_monitors():
 
 @app.route('/api/v1/dollar/unit/<string:key_monitor>', methods=["GET"])
 def get_by_monitor(key_monitor: str):
-    response = jsonify(api.get_monitor(key_monitor))
-    return response
-
-@app.route('/api/v1/dollar/<string:section_dollar>', methods=["GET"])
-def get_monitor_by_section(section_dollar: str):
-    response = jsonify(api.categorize_monitors(section_dollar))
-    return response
-
-@app.route('/api/v1/dollar/<string:section_dollar>/<string:key_monitor>', methods=["GET"])
-def get_monitor_by_section_and_key(section_dollar: str, key_monitor: str):
-    response = jsonify(api.categorize_monitors(section_dollar, key_monitor))
+    response = jsonify(api.get_information_dollar(key_monitor))
     return response
 
 @app.route('/api/v1/dollar/tb/<string:value>/<string:key_monitor>', methods=["GET"])
 def convertion_to_dollar(value: str, key_monitor: str):
-    try:
-        value = float(value)
-
-        price = api.get_dollar(key_monitor)
-        result = value * float(price) if not type(price) == dict else price
-        return jsonify({'value_to_bs': result, "information": api.get_monitor(key_monitor)})
-    except ValueError:
-        return jsonify({'message': f'Cannot GET information the value: {value}'})
+    response = jsonify(api.get_price_converted('VES', value, key_monitor))
+    return response
     
 @app.route('/api/v1/dollar/td/<string:value>/<string:key_monitor>', methods=["GET"])
 def convertion_to_bs(value: str, key_monitor: str):
-    try:
-        value = float(value)
-
-        price = api.get_dollar(key_monitor)
-        result = value / float(price) if not type(price) == dict else price
-        return jsonify({'value_to_dollar': result, "information": api.get_monitor(key_monitor)})
-    except ValueError:
-        return jsonify({'message': f'Cannot GET information the value: {value}'})
+    response = jsonify(api.get_price_converted('USD', value, key_monitor))
+    return response
--- src/obtaining.py
+++ src/obtaining.py
@@ -1,60 +1,48 @@
-from pyDolarVenezuela import Monitor
-from src.variables import RANGES, CATEGORIES
-from src.tools import get_error
+from pyDolarVenezuela.pages import CriptoDolar
+from pyDolarVenezuela.tools import TimeDollar
+from pyDolarVenezuela import Monitor, currency_converter
 
-monitor = Monitor()
+# from src.variables import RANGES, CATEGORIES
+# from src.tools import get_error
 
-def check_key(key: str) -> bool:
-    return key in RANGES
+# def check_key(key: str) -> bool:
+#     return key in RANGES
 
-class Api:
-    def __init__(self) -> None:
-        self.categorized_dict = {}
-
-    def categorize_monitors(self, section_dollar: str, key_monitor: str = None):
-        for key, value in monitor.get_value_monitors().items():
-
-            for category, values in CATEGORIES.items():
-                if key in values:
-                    break
-            else:
-                category = 'fecha'
-
-            if category not in self.categorized_dict:
-                self.categorized_dict[category] = {}
-            self.categorized_dict[category][key] = value
-
-        if section_dollar not in self.categorized_dict:
-            return get_error("section_dollar", section_dollar)
-        if key_monitor is not None and key_monitor not in self.categorized_dict[section_dollar]:
-            return get_error("key_monitor", key_monitor)
-        
-        return (self.categorized_dict[section_dollar][key_monitor] if key_monitor in self.categorized_dict[section_dollar]
-                else self.categorized_dict[section_dollar])
+class NewApi:
+    def __init__(self, Provider=CriptoDolar) -> None:
+        self.monitor = Monitor(Provider)
+        self.time    = TimeDollar()
     
     def get_all_monitors(self):
-        monitors = monitor.get_value_monitors()
+        monitors = self.monitor.get_value_monitors()
+        datetime = self.time.get_time_zone()
+
         result = {
-            "datetime": monitors.pop("datetime"),
+            "datetime": datetime,
             "monitors": monitors
         }
+
         return result
     
-    def get_dollar(self, key_monitor: str):
-        if not check_key(key_monitor):
-            return get_error("key_monitor", key_monitor)
+    def get_information_dollar(self, monitor_code: str):
         try:
-            result = monitor.get_value_monitors(key_monitor, name_property='price')
+            result = self.monitor.get_value_monitors(monitor_code)
             return result
         except Exception as e:
             return {'error': f'An error occurred: {str(e)}'}
-        
-    def get_monitor(self, key_monitor: str):
-        if not check_key(key_monitor):
-            return get_error("key_monitor", key_monitor)
+    
+    def get_price_dollar(self, monitor_code: str):
         try:
-            result = monitor.get_value_monitors(key_monitor)
+            result = self.monitor.get_value_monitors(monitor_code, name_property='price')
             return result
+        except Exception as e:
+            return {'error': f'An error occurred: {str(e)}'}
+    
+    def get_price_converted(self, type: str, value, monitor_code: dict):
+        try:
+            monitor = self.get_information_dollar(monitor_code)
+
+            return currency_converter(type, value, monitor)
         except Exception as e:
             return {'error': f'An error occurred: {str(e)}'}
         

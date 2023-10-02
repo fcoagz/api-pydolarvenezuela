75ed9210c41b863602c8ca3f3f456972a0c7eb0f
fcoagz <grihardware@gmail.com>
2023-10-02 16:48:02 -0400

    adding more function

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-from src.obtaining import NewApi
-from src.exceptions import page_not_found, internal_server_error
+from src.obtaining import pyDolarVenezuelaApi
+from src.exceptions import (
+    forbidden,
+    page_not_found,
+    internal_server_error,
+    gateway_timeout
+)
 
-api = NewApi()
+api = pyDolarVenezuelaApi()
 app = Flask(__name__)
 CORS(app)
 
 # error handling
+app.register_error_handler(403, forbidden)
 app.register_error_handler(404, page_not_found)
 app.register_error_handler(500, internal_server_error)
+app.register_error_handler(504, gateway_timeout)
 
 @app.route('/')
 def index():
@@ -21,9 +28,19 @@ def get_monitors():
     response = jsonify(api.get_all_monitors())
     return response
 
+@app.route('/api/v1/dollar/<string:page>', methods=["GET"])
+def get_monitor_by_page(page: str):
+    response = jsonify(api.get_specific_page_monitors(page))
+    return response
+
+@app.route('/api/v1/dollar/<string:page>/<string:key_monitor>', methods=["GET"])
+def get_monitor_by_page_and_monitor(page: str, key_monitor: str):
+    response = jsonify(api.get_information_dollar(page, key_monitor))
+    return response
+
 @app.route('/api/v1/dollar/unit/<string:key_monitor>', methods=["GET"])
 def get_by_monitor(key_monitor: str):
-    response = jsonify(api.get_information_dollar(key_monitor))
+    response = jsonify(api.get_information_dollar(monitor_code=key_monitor))
     return response
 
 @app.route('/api/v1/dollar/tb/<string:value>/<string:key_monitor>', methods=["GET"])
--- src/obtaining.py
+++ src/obtaining.py
@@ -1,21 +1,22 @@
-from pyDolarVenezuela.pages import CriptoDolar
-from pyDolarVenezuela.tools import TimeDollar
-from pyDolarVenezuela import Monitor, currency_converter
+from pyDolarVenezuela.pages import BCV, CriptoDolar, ExchangeMonitor, iVenezuela
+from pyDolarVenezuela import currency_converter, getdate, pages 
+from pyDolarVenezuela import Monitor
 
-# from src.variables import RANGES, CATEGORIES
-# from src.tools import get_error
+provider_dict = {
+        "criptodolar": CriptoDolar,
+        "bcv": BCV,
+        "exchangemonitor": ExchangeMonitor,
+        "ivenezuela": iVenezuela
+} 
 
-# def check_key(key: str) -> bool:
-#     return key in RANGES
+class pyDolarVenezuelaApi:
+    def __init__(self) -> None:
+        self.monitor = Monitor(CriptoDolar) # DEFAULT
 
-class NewApi:
-    def __init__(self, Provider=CriptoDolar) -> None:
-        self.monitor = Monitor(Provider)
-        self.time    = TimeDollar()
-    
-    def get_all_monitors(self):
-        monitors = self.monitor.get_value_monitors()
-        datetime = self.time.get_time_zone()
+    def get_all_monitors(self, provider: pages.Monitor = CriptoDolar):
+        get_monitor = Monitor(provider)
+        monitors = get_monitor.get_value_monitors()
+        datetime = getdate()
 
         result = {
             "datetime": datetime,
@@ -24,12 +25,42 @@ class NewApi:
 
         return result
     
-    def get_information_dollar(self, monitor_code: str):
+    def get_specific_page_monitors(self, page: str):
         try:
-            result = self.monitor.get_value_monitors(monitor_code)
-            return result
+            provider = provider_dict[page]
+
+            if provider.name == "Exchange Monitor":
+                return {'error': 'Utilice la ruta /exchangemonitor/<monitor>. No podemos ofrecerte todos los monitores en un solo lugar.'}
+            else:
+                result = self.get_all_monitors(provider)
+                return result
         except Exception as e:
             return {'error': f'An error occurred: {str(e)}'}
+        
+    def get_information_dollar(self, page: str = None, monitor_code: str = None):
+        if not page:
+            try:
+                result = self.monitor.get_value_monitors(monitor_code)
+                return result
+            except Exception as e:
+                return {'error': f'An error occurred: {str(e)}'}
+        else:
+            try:
+                provider = provider_dict[page]
+                
+                result = self.get_all_monitors(provider)
+                if provider.name == "Exchange Monitor":
+                    get_monitor = Monitor(provider)
+                    monitor = get_monitor.get_value_monitors(monitor_code)
+                    
+                    return monitor
+                if monitor_code in result['monitors']:
+                    return result['monitors'][monitor_code]
+                else:
+                    return {'error': 'No se encontró el monitor al que quieres acceder'}
+            except Exception as e:
+                return {'error': f'An error occurred: {str(e)}'}
+            
     
     def get_price_dollar(self, monitor_code: str):
         try:
@@ -45,4 +76,3 @@ class NewApi:
             return currency_converter(type, value, monitor)
         except Exception as e:
             return {'error': f'An error occurred: {str(e)}'}
-        

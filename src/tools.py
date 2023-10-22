51d8201e45e75daa1756ae62fbeea156abfff332
fcoagz <grihardware@gmail.com>
2023-10-21 20:31:24 -0400

    ~

--- src/tools.py
+++
@@ -1,9 +0,0 @@
-import functools
-import operator
-
-
-def functools_reduce_iconcat(lst):
-    return functools.reduce(operator.iconcat, lst, [])
-
-def get_error(name, value):
-    return {'error': f'Invalid {name}: {value}'}

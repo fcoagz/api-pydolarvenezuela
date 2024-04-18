45f2d147147bcc2c27851e646e70c91c79e4f060
fcoagz <grihardware@gmail.com>
2024-04-18 15:20:03 -0400

    ~

--- src/routes/monitors.py
+++ src/routes/monitors.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, jsonify
 from ..utils.pydolarvenezuela import pyDolarVenezuelaApi
-from ..utils.cache import cache
 
 route = Blueprint('monitors', __name__)
 api   = pyDolarVenezuelaApi()
@@ -9,25 +8,17 @@ api   = pyDolarVenezuelaApi()
 def get_monitor_by_page_or_monitor(currency: str):
     page    = request.args.get('page', 'criptodolar')
     monitor = request.args.get('monitor', None)
-    key     = f'{currency}:{page}:{monitor}'
-    
-    if not cache.get_data(key):
-        if monitor:
-            response = jsonify(api.get_information_monitor(currency, page, monitor)), 200
-            cache.set_data(key, response)
-        else:
-            response = jsonify(api.get_specific_page_monitors(page, currency)), 200
-            cache.set_data(key, response)
-    return cache.get_data(key)
+
+    if monitor:
+        response = jsonify(api.get_information_monitor(currency, page, monitor)), 200
+    else:
+        response = jsonify(api.get_specific_page_monitors(page, currency)), 200
+    return response
 
 @route.get('/api/v1/<string:currency>/unit/<string:key_monitor>')
 def get_by_monitor(currency: str, key_monitor: str):
-    key = f'{currency}:unit:{key_monitor}'
-
-    if not cache.get_data(key):
-        response = jsonify(api.get_information_monitor(currency, monitor_code=key_monitor)), 200
-        cache.set_data(key, response)
-    return cache.get_data(key)
+    response = jsonify(api.get_information_monitor(currency, monitor_code=key_monitor)), 200
+    return response
 
 @route.get('/api/v1/<string:currency>/conversion')
 def value_conversion(currency: str):

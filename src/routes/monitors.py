cb75f55af6b224b17c186d14d4b7f3d0e2dbc781
fcoagz <grihardware@gmail.com>
2024-06-06 15:08:04 -0400

    Correcting code and handle errors

--- src/routes/monitors.py
+++ src/routes/monitors.py
@@ -4,21 +4,29 @@ from ..utils.pydolarvenezuela import pyDolarVenezuelaApi
 route = Blueprint('monitors', __name__)
 api   = pyDolarVenezuelaApi()
 
+def handle_response(response):
+    """Maneja la respuesta del API."""
+    if response.get('error'):
+        return jsonify(response), 400
+    else:
+        return jsonify(response), 200
+
 @route.get('/api/v1/<string:currency>')
 def get_monitor_by_page_or_monitor(currency: str):
     page    = request.args.get('page', 'criptodolar')
     monitor = request.args.get('monitor', None)
 
     if monitor:
-        response = jsonify(api.get_information_monitor(currency, page, monitor)), 200
+        response = api.get_information_monitor(currency, page, monitor)
     else:
-        response = jsonify(api.get_specific_page_monitors(page, currency)), 200
-    return response
+        response = api.get_specific_page_monitors(page, currency)
+    
+    return handle_response(response)
 
 @route.get('/api/v1/<string:currency>/unit/<string:key_monitor>')
 def get_by_monitor(currency: str, key_monitor: str):
-    response = jsonify(api.get_information_monitor(currency, monitor_code=key_monitor)), 200
-    return response
+    response = api.get_information_monitor(currency, monitor_code=key_monitor)
+    return handle_response(response)
 
 @route.get('/api/v1/<string:currency>/conversion')
 def value_conversion(currency: str):

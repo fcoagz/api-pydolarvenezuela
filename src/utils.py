40abad33ae0261f83082ed4c965de93ee577a4c7
Francisco Griman <grihardware@gmail.com>
2024-08-02 19:55:57 -0400

    Add currency providers and update schedule

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+providers = ['criptodolar', 'bcv', 'italcambio', 'alcambio', 'dolartoday', 'enparalelovzla']
+
+currencies_dict = {
+    'dollar': 'usd',
+    'euro': 'eur'
+}
+
+providers_dict = {
+    'Cripto Dolar': 'criptodolar',
+    'Banco Central de Venezuela': 'bcv',
+    'Italcambio': 'italcambio',
+    'Al Cambio': 'alcambio',
+    'Dolar Today': 'dolartoday',
+    'EnParaleloVzla': 'enparalelovzla'
+}
+
+update_schedule = {
+    'enparalelovzla': [('08:55', '09:55'), ('12:55', '13:55')],
+    'dolartoday': [('08:55', '09:55'), ('12:30', '13:30'), ('16:55', '17:55')],
+    'alcambio': [('08:55', '09:55'), ('12:55', '13:55'), ('16:55', '17:55')],
+    'bcv': [('16:00', '17:55')]
+}

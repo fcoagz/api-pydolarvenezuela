e3b01551e4b664b6e70079d242eef7ecffdbdbb0
Francisco Griman <grihardware@gmail.com>
2024-08-02 19:56:52 -0400

    Add admin routes for user management

---
+++ src/routes/admin.py
@@ -0,0 +1,69 @@
+from flask import Blueprint, request, jsonify
+from sqlalchemy.orm import sessionmaker
+from ..consts import TOKEN_SECRET
+from ..data.engine import engine
+from ..data.services import (
+    create_user as create_user_,
+    modificate_user as modificate_user_,
+    delete_user as delete_user_,
+    get_users as get_users_
+)
+
+route   = Blueprint('admin', __name__)
+session = sessionmaker(bind=engine)()
+
+@route.get('/api/admin/get_users')
+def get_users_route():
+    _token_admin = request.headers.get('Authorization')
+    
+    if _token_admin != TOKEN_SECRET:
+        return jsonify({"error": "Token inválido."}), 401
+    
+    users = get_users_(session)
+    return jsonify(users), 200
+
+@route.post('/api/admin/create_user')
+def create_user():
+    _token_admin = request.headers.get('Authorization')
+    name = request.form.get('name')
+    
+    if _token_admin != TOKEN_SECRET:
+        return jsonify({"error": "Token inválido."}), 401
+    
+    if name:
+        try:
+            create_user_(session, name)
+            return jsonify({"message": "Usuario creado exitosamente."}), 201
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
+        
+    return jsonify({"error": "Faltan datos."}), 400
+
+@route.put('/api/admin/modificate_user')
+def modificate_user():
+    _token_admin = request.headers.get('Authorization')
+    id = request.form.get('id')
+    is_premium = request.form.get('is_premium')
+    
+    if _token_admin != TOKEN_SECRET:
+        return jsonify({"error": "Token inválido."}), 401
+    
+    if all([id, is_premium]):
+        modificate_user_(session, id, is_premium)
+        return jsonify({"message": "Usuario modificado exitosamente."}), 200
+    
+    return jsonify({"error": "Faltan datos."}), 400
+
+@route.delete('/api/admin/delete_user')
+def delete_user():
+    _token_admin = request.headers.get('Authorization')
+    id = request.form.get('id')
+    
+    if _token_admin != TOKEN_SECRET:
+        return jsonify({"error": "Token inválido."}), 401
+    
+    if id:
+        delete_user_(session, id)
+        return jsonify({"message": "Usuario eliminado exitosamente."}), 200
+    
+    return jsonify({"error": "Faltan datos."}), 400

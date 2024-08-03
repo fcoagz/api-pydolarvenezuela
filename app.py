c510c8fb4128f4bdd8bf7ebe53b71a7c1f410db8
Francisco Griman <grihardware@gmail.com>
2024-08-02 20:16:54 -0400

    Update app with scheduler, and admin routes

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+import logging 
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 from flask_cors import CORS
 from flasgger import Swagger
 
-from src.routes import index, monitors
+from src.core import limiter
+from src.consts import TIMEOUT, GETLOGS
+from src import cron
+from src.routes import index, monitors, admin
 from src.exceptions import (
     forbidden,
     page_not_found,
@@ -10,8 +15,18 @@ from src.exceptions import (
     gateway_timeout
 )
 
+if GETLOGS:
+    logging.basicConfig(filename='logs.log', level=logging.INFO)
+
+# scheduler
+scheduler = BackgroundScheduler(daemon=True)
+scheduler.add_job(cron.job, 'interval', minutes=TIMEOUT)
+scheduler.start()
+
+# app
 app = Flask(__name__)
 CORS(app)
+limiter.init_app(app)
 swagger = Swagger(app, template_file='src/swagger.yaml')
 
 # error handling
@@ -23,3 +38,8 @@ app.register_error_handler(504, gateway_timeout)
 # routes
 app.register_blueprint(index.route)
 app.register_blueprint(monitors.route)
+app.register_blueprint(admin.route)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+    scheduler.shutdown()

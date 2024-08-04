1aed5c9dc545e676934a8efedb6ebc4459ab86de
Francisco Griman <grihardware@gmail.com>
2024-08-04 15:43:05 -0400

    chore: Format last update date in results

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+from typing import List, Dict, Any
+
 providers = ['criptodolar', 'bcv', 'italcambio', 'alcambio', 'dolartoday', 'enparalelovzla']
 
 currencies_dict = {
@@ -19,3 +21,15 @@ update_schedule = {
     'alcambio': [('08:55', '09:55'), ('12:55', '13:55'), ('16:55', '17:55')],
     'bcv': [('16:00', '17:55')]
 }
+
+def format_last_update(results: List[Dict[str, Any]]) -> None:
+    """
+    Formatea la fecha de la última actualización de los resultados.
+
+    - results: Lista de resultados.
+    """
+    for result in results:
+        if 'last_update' in result:
+            last_update = result['last_update']
+            formatted_last_update = last_update.strftime('%d/%m/%Y, %I:%M %p')
+            result.update({'last_update': formatted_last_update})

f3cd3e61acaaa4df46f2296b670281492e552f46
Francisco Griman <grihardware@gmail.com>
2024-09-18 22:01:07 -0400

    Remove imports and functions

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
-from pyDolarVenezuela.models import Monitor, HistoryPrice
-from typing import List, Union
-from .consts import TIME_ZONE
+# from pyDolarVenezuela.models import Monitor, HistoryPrice
+# from typing import List, Union
+# from .consts import TIME_ZONE
 
 providers = ['criptodolar', 'bcv', 'italcambio', 'alcambio', 'dolartoday', 'enparalelovzla']
 
@@ -24,14 +24,14 @@ update_schedule = {
     'bcv': [('16:00', '17:55')]
 }
 
-# https://stackoverflow.com/questions/414952/sqlalchemy-datetime-timezone
-def format_last_update(results: List[Union[Monitor, HistoryPrice]]) -> List[Union[Monitor, HistoryPrice]]:
-    """
-    Formatea la fecha de la última actualización de los resultados.
+# # https://stackoverflow.com/questions/414952/sqlalchemy-datetime-timezone
+# def format_last_update(results: List[Union[Monitor, HistoryPrice]]) -> List[Union[Monitor, HistoryPrice]]:
+#     """
+#     Formatea la fecha de la última actualización de los resultados.
 
-    - results: Lista de resultados.
-    """
-    for result in results:
-        last_update_obj = result.last_update.astimezone(TIME_ZONE)
-        result.last_update = last_update_obj.strftime('%d/%m/%Y, %I:%M %p')
-    return results
+#     - results: Lista de resultados.
+#     """
+#     for result in results:
+#         last_update_obj = result.last_update.astimezone(TIME_ZONE)
+#         result.last_update = last_update_obj.strftime('%d/%m/%Y, %I:%M %p')
+#     return results
